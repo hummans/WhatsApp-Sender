efd3f312adfcb5816d5c5951bdea2bb6230c7fef
Felipe Tiozo <felipetiozo10@gmail.com>
2018-05-13 10:18:20 -0300

    testing heroku

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function searchById(id){
-  var count = -1
-  for(var i = 0;i < history.length;i++){
-    count++
-    if(history[i].messageid==id){
-      break
-    }
-    if(i==history.length-1){
-      return 'not found'      
-    }  
-  }
-  return history[count]
-}
-
-console.log(searchById(5454))
+console.log('aas')
